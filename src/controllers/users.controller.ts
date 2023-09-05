df82dd3e84d128eb74996867fa9eddeb13078b04
Adnan <adnanghannam21@gmail.com>
2023-09-05 20:44:00 +0300

    add 3 user endpoints

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 import env from "../config/env.config";
-import { httpSuccess } from "../helpers/response.helpers";
+import { httpError, httpSuccess } from "../helpers/response.helpers";
 import { Types } from "mongoose";
 import db from "../models/models";
 import { tryHandle } from "../helpers/controller.helpers";
@@ -98,16 +98,49 @@ const removeByIdEndpoint: RequestHandler = async (req, res) => {
     });
 };
 
-const getCollectionEndpoint: RequestHandler = (req, res) => {
+const getCollectionEndpoint: RequestHandler = async (req, res) => {
+    const { loginUser } = res.locals;
 
+    const collection = await db.Collection.findById(loginUser.userCollection).populate("articles");
+
+    res.status(200).json(httpSuccess(collection));
 };
 
-const addToCollectionEndpoint: RequestHandler = (req, res) => {
+const addToCollectionEndpoint: RequestHandler = async (req, res) => {
+    const { loginUser: user, article } = res.locals;
 
+    const collection = (await db.Collection.findById(user.userCollection))!;
+
+    // Check if exists
+    if (collection.articles.indexOf(article.id) != -1) {
+        res.status(400)
+            .json(httpError("You already have this post in your collection"));
+        return;
+    }
+
+    // Check for limit
+    if (collection.articles.length > 10) {
+        res.status(400)
+            .json(httpError("You've reached you limit"));
+        return;
+    }
+
+    tryHandle(res, async () => {
+        collection.articles = [...collection.articles, article.id ];
+        await collection.save();
+        
+        res.status(204).end();
+    });
 };
 
-const removeFromCollectionEndpoint: RequestHandler = (req, res) => {
+const removeFromCollectionEndpoint: RequestHandler = async (req, res) => {
+    const { loginUser: user, post } = res.locals;
+    const collection = await db.Collection.findById(user?.userCollection);
 
+    tryHandle(res, async () => {
+        await collection?.updateOne({ $pull: { posts: post.id }});
+        res.status(204).end();
+    });
 };
 
 const controller = {
